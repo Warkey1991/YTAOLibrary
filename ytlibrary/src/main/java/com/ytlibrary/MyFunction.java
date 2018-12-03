d745aa98dab39c16f5594516e11cd26acea3ff9b
CNAD666 <526250922@qq.com>
2018-12-03 17:58:42 +0800

    首次上传

---
+++ ytlibrary/src/main/java/com/ytlibrary/MyFunction.java
@@ -0,0 +1,14 @@
+package com.ytlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by ytao on 2018/11/21
+ * Describe: 测试库
+ */
+public class MyFunction {
+    public static void showToast(Context context, String message){
+        Toast.makeText(context, message + "功能库测试", Toast.LENGTH_LONG).show();
+    }
+}
